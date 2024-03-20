ba4e55b1fe4134a4c1fe2e33a32b545d13cd39cf
risheelg01 <63624190+risheelg01@users.noreply.github.com>
2024-03-19 21:02:12 -0500

    added starter apps
    starter apps

---
+++ backend/server.py
@@ -0,0 +1,27 @@
+#from flask import Flask, render_template, request
+import pandas as pd
+import requests as rq
+
+from flask import Flask, abort, current_app, request, render_template
+import json
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/', methods=['POST'])
+def stock_tracker():
+
+    json_data = json.dumps(0)
+    response = app.response_class(
+        response=json_data,
+        status=200,
+        mimetype='application/json'
+    )
+
+    return response
+
+
+if __name__ == '__main__':
+    app.run(host = '127.0.0.1', port = 5001, debug = True)
